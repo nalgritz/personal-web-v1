4da477266a96c04faae08095c51382f5e5e1d020
nalgritz <nalgritz9270@gmail.com>
2016-09-08 13:24:26 +0800

    add work descriptions and font size

--- js/controllers/works_controller.js
+++ js/controllers/works_controller.js
@@ -2,15 +2,15 @@ app.controller('WorksCtrl', ['$scope', function($scope){
 
   $scope.works = [{
     title: "Dodge Hell",
-    subtitle: "Minigame Revival",
+    subtitle: "Minigame Revival - pure frontend game finished in 3-week learning",
     img_url: "./img/project1_dodge_hell.png"
   }, {
     title: "Split",
-    subtitle: "Taxi Sharing Platform",
+    subtitle: "Taxi Sharing Platform - a taxi sharing platform using nodejs, mongodb and jquery with Google geo-location service",
     img_url: "./img/project2_split.png"
   }, {
     title: "Spoonfeed",
-    subtitle: "A news aggregation Platform",
+    subtitle: "A news aggregation service made with classmates which empowered me good version control skill",
     img_url: "./img/project3_spoonfeed.png"
   }, {
     title: "SimplyHome",
